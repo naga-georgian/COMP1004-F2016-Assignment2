65407a207fbce4c8152d3f453e7f7f836a00ca58
naga-georgian <nagavenkatasarat.rimmalapudi@mygeorgian.ca>
2016-10-15 08:23:15 -0400

    Added Splash Screen and Variables for Calculations

--- COMP1004-F2016-Assignment2/SharpAutoForm.cs
+++ COMP1004-F2016-Assignment2/SharpAutoForm.cs
@@ -19,6 +19,17 @@ namespace COMP1004_F2016_Assignment2
 {
     public partial class autoCentreForm : Form
     {
+
+        // Constant Variables
+        const decimal TAX_RATE_Decimal = .13m;
+        // Accessories
+        const decimal STEREO_SYSTEM_Decimal = 425.76m;
+        const decimal LEATHER_INTERIOR_Decimal = 987.41m;
+        const decimal COMPUTER_NAVIGATION_Decimal = 1741.23m;
+        // Exterior Finish
+        const decimal STANDARD_Decimal = 0.00m;
+        const decimal PERALIZED_Decimal = 345.72m;
+        const decimal CUSTOMIZED_DETAILING_Decimal = 599.99m;
         public autoCentreForm()
         {
             InitializeComponent();
