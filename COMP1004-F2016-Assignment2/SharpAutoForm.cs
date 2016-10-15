39e1dd0e178ad24ca8976146128e207217afdfad
naga-georgian <nagavenkatasarat.rimmalapudi@mygeorgian.ca>
2016-10-15 08:29:14 -0400

    Completed Calculate function

--- COMP1004-F2016-Assignment2/SharpAutoForm.cs
+++ COMP1004-F2016-Assignment2/SharpAutoForm.cs
@@ -58,9 +58,129 @@ namespace COMP1004_F2016_Assignment2
         {
             CalculateFunction();
         }
-            private void CalculateFunction()
-        {
 
+        private void CalculateFunction()
+        {
+            // Local variables
+            decimal carSalesPriceDecimal;
+            decimal tradeInAllowanceDecimal;
+            decimal accessoriesExteriorSubTotalDecimal = 0.00m;
+            decimal subTotalDecimal;
+            decimal totalDecimal;
+            decimal salesTaxDecimal;
+            decimal amountDueDecimal;
+            try
+            {
+                carSalesPriceDecimal = decimal.Parse(carSalesPriceTextBox.Text);
+                // Car sales price cannot be below or equal to 0
+                if (carSalesPriceDecimal <= 0)
+                {
+                    MessageBox.Show("The value inputted in the Car Sale Price must be above zero.", "Value too small Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    carSalesPriceDecimal = 0.00m;
+                    carSalesPriceTextBox.Select();
+                    carSalesPriceTextBox.Focus();
+                }
+                else
+                {
+                    try
+                    {
+                        tradeInAllowanceDecimal = decimal.Parse(tradeInAllowanceTextBox.Text);
+                        // Trade in value cannot be below 0
+                        if (tradeInAllowanceDecimal < 0)
+                        {
+                            MessageBox.Show("The value inputted in Trade-On Allowance must not be a negative value.", "Value too small Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            tradeInAllowanceDecimal = 0.00m;
+                            tradeInAllowanceTextBox.Select();
+                            tradeInAllowanceTextBox.Focus();
+                        }
+                        else
+                        {
+                            // Switch to check which radio button has been clicked
+                            switch (selectedRadioButton.Name)
+                            {
+                                case "peralizedRadioButton":
+                                    accessoriesExteriorSubTotalDecimal = PERALIZED_Decimal;
+                                    break;
+                                case "customizedDetailingRadioButton":
+                                    accessoriesExteriorSubTotalDecimal = CUSTOMIZED_DETAILING_Decimal;
+                                    break;
+                                default:
+                                    accessoriesExteriorSubTotalDecimal = STANDARD_Decimal;
+                                    break;
+                            }
+                            // If check box is checked, add to subtotal
+                            if (stereoSystemCheckBox.Checked)
+                            {
+                                accessoriesExteriorSubTotalDecimal += STEREO_SYSTEM_Decimal;
+                            }
+                            if (leatherInteriorCheckBox.Checked)
+                            {
+                                accessoriesExteriorSubTotalDecimal += LEATHER_INTERIOR_Decimal;
+                            }
+                            if (computerNavigationCheckBox.Checked)
+                            {
+                                accessoriesExteriorSubTotalDecimal += COMPUTER_NAVIGATION_Decimal;
+                            }
+                            // Calculations
+                            subTotalDecimal = accessoriesExteriorSubTotalDecimal + carSalesPriceDecimal;
+                            salesTaxDecimal = subTotalDecimal * TAX_RATE_Decimal;
+                            totalDecimal = subTotalDecimal + salesTaxDecimal;
+                            amountDueDecimal = totalDecimal - tradeInAllowanceDecimal;
+
+                            // Add all variables to the TextBoxes
+                            accessoriesFinishTextBox.Text = accessoriesExteriorSubTotalDecimal.ToString("c");
+                            subtotalTextBox.Text = subTotalDecimal.ToString("c");
+                            salesTaxTextBox.Text = salesTaxDecimal.ToString("c");
+                            totalTextBox.Text = totalDecimal.ToString("c");
+                            amountDueTextBox.Text = amountDueDecimal.ToString("c");
+
+
+                        }
+                    }
+                    // Catch Format Exceptions
+                    catch (FormatException tradeInAllowanceFormatException)
+                    {
+                        MessageBox.Show("The value inputted in Trade-In Allowance must be a numeric value.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        tradeInAllowanceTextBox.Select();
+                        tradeInAllowanceTextBox.Focus();
+                    }
+                    // Catch Overflow Exceptions
+                    catch (OverflowException tradeInAllowanceOverflowException)
+                    {
+                        MessageBox.Show("The value inputted in Trade-In Allowance is out of range.", "Overflow Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        tradeInAllowanceTextBox.Select();
+                        tradeInAllowanceTextBox.Focus();
+                    }
+                    // Catch any exceptions that weren't caught previously
+                    catch (Exception tradeInAllowanceGenericException)
+                    {
+                        MessageBox.Show("The value in Trade-In Allowance has caused an unknown error. Error: " + tradeInAllowanceGenericException.Message, "Exception Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        tradeInAllowanceTextBox.Select();
+                        tradeInAllowanceTextBox.Focus();
+                    }
+                }
+            }
+            // Catch Format Exceptions
+            catch (FormatException carSalesFormatException)
+            {
+                MessageBox.Show("The value inputted in the Car Sale Price must be a numeric value.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                carSalesPriceTextBox.Select();
+                carSalesPriceTextBox.Focus();
+            }
+            // Catch Overflow Exceptions
+            catch (OverflowException carSalesOverflowException)
+            {
+                MessageBox.Show("The value inputted in the Car Sale Price is out of range.", "Overflow Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                carSalesPriceTextBox.Select();
+                carSalesPriceTextBox.Focus();
+            }
+            // Catch any exceptions that weren't caught previously
+            catch (Exception carSalesGenericException)
+            {
+                MessageBox.Show("The value in the Car Sale Price has caused an unknown error. Error: " + carSalesGenericException.Message, "Exception Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                carSalesPriceTextBox.Select();
+                carSalesPriceTextBox.Focus();
+            }
         }
     }
 }
